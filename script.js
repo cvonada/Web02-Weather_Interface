90c929cce5e940ea74351bb880e3f8e277069f65
Courtney Vonada <courtneyvonada@gmail.com>
2021-02-15 18:38:27 -0500

    Connected API

---
+++ script.js
@@ -0,0 +1,20 @@
+const lat = '40.659019',
+    long = '-74.002245',
+    api = '7352a2ba8615067479a7102a940ff88e',
+    unit = 'imperial',
+    url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${api}&units=${unit}`;
+
+function updateUI(data) {
+    console.log(data);
+}
+
+fetch(url)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    return updateUI(data);
+})
+.catch(function(error) {
+    console.log(error);
+});
